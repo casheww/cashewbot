28ccfc41eb570a2eeb5f6b3ee0105a66fe434af6
casheww <51793722+casheww@users.noreply.github.com>
2020-07-15 14:01:43 +0100

    Update custom_checks.py

--- custom_checks.py
+++ custom_checks.py
@@ -1,8 +1,48 @@
 from discord.ext.commands import check
-import botdb
 
 
 def is_celery():
     def predicate(ctx):
-        return ctx.author.id == 508862828103467010
+        with open("_keys.gitignore", "r") as f:
+            kelb_id = f.read()
+        return ctx.author.id == kelb_id
+    return check(predicate)
+
+
+def uno_enabled():
+    def predicate(ctx):
+        return ctx.guild.id in ctx.bot.uno_guilds
+    return check(predicate)
+
+
+def uno_game_in_progress():
+    def predicate(ctx):
+        return ctx.guild.id in ctx.bot.uno_games.keys()
+    return check(predicate)
+
+
+def uno_game_not_in_progress():
+    def predicate(ctx):
+        return ctx.guild.id not in ctx.bot.uno_games.keys()
+    return check(predicate)
+
+
+def uno_player_channel():
+    def predicate(ctx):
+        game = ctx.bot.uno_games[ctx.guild.id]
+        current_player = game.get_player(ctx.author.id)
+
+        if current_player is None:      # member is not in Uno game
+            return False
+
+        return current_player.channel_id == ctx.channel.id
+    return check(predicate)
+
+
+def uno_current_player():
+    def predicate(ctx):
+        game = ctx.bot.uno_games[ctx.guild.id]
+        current_player = game.player_list[0]
+
+        return current_player.member_id == ctx.author.id
     return check(predicate)
